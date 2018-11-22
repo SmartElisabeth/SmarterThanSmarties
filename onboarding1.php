4e3eb08fe54a621019ac41af13f5bccd2d91f17f
SmartElisabeth <44946979+SmartElisabeth@users.noreply.github.com>
2018-11-22 16:19:56 +0100

    onboarding_22.11_Eli
    hot shit

---
+++ onboarding1.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+
+<?php
+include("PHP/connect.php");
+include("PHP/Mysql.class.php");
+include("PHP/MysqlStatement.class.php");
+?>
+
+<html>
+<head>
+    <!-- Include meta tag to ensure proper rendering and touch zooming -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Include jQuery Mobile stylesheets -->
+    <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+    <!-- Include the jQuery library -->
+    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <!-- Include the jQuery Mobile library -->
+    <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+    <link href="CSS/style_onboarding1.css" rel="stylesheet">
+</head>
+<body>
+
+
+
+<?php
+/**
+ * DEBUG:
+ * echo $MysqlStatement_delete->sql; Anzeige SQL Statement
+ */
+
+/* get the mysql object */
+$Mysql = new Mysql();
+
+
+/* settings data if a new version is out there and the std STP ID */
+//SELECT * FROM user WHERE name="matthias" AND pw="1234";
+
+$sql = "SELECT * FROM user WHERE username=:0 AND userpassword=:1";
+$MysqlStatement_select = $Mysql->getMysqlStatement($sql);
+$MysqlStatement_select->execute($_POST[name], $_POST[pw]);
+
+?>
+
+
+<div data-role="page" id="LandingPage">
+    <div data-role="main" class="ui-content">
+
+        <!--  <div class="background">
+            <img src="image/4.png" title="SmartGlassed_BG" > -->
+
+        <a href="onboarding2.php" class="ui-btn" data-ajax="false" >NEXT</a>
+
+
+        </div>
+
+
+    </div>
+
+</div>
+
+
+ <?php 
+
+        if ($MysqlStatement_select->num_rows>=1)
+        {
+        echo "it worked";
+
+        header("Location: http://localhost:8888/SmarterThanSmarties/login.php");
+        
+        }
+
+?>
+
+</div>
+
+
+
+
+</body>
+</html>
+
